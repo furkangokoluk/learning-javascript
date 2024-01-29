bedcb1e254e27d9d3aca43fea177af140bf8f275
Furkan Gökoluk <furkangokoluk@outlook.com>
2024-01-29 22:16:01 +0300

    first commit

---
+++ js/JS Çıktı Verme/app.js
@@ -0,0 +1,11 @@
+// Document write ile cikti verme.
+document.write("Gümüşhane Üniversitesi");
+
+document.write("Mühendislik ve Doğa Bilimleri Fakültesi");
+// Console log ile cikti verme.
+console.log("Yazılım Mühendisliği");
+console.log("Sınıfı: 3");
+console.log({name:"Furkan", surname:"Gökoluk"});
+
+// Allert popup ile cikti verme.
+window.alert("Üniversite Öğrenci Bilgilerimiz.");
